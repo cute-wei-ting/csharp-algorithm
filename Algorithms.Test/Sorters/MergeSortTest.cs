0a24999741654b7e2babd8d51b99c9448a958f84
skye_tseng <a0919111125@gmail.com>
2022-07-19 16:29:42 +0800

    Feat: 新增MergeSort

---
+++ Algorithms.Test/Sorters/MergeSortTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Sorters;
+using Xunit;
+
+namespace Algorithms.Test.Sorters
+{
+    public class MergeSortTest
+    {
+        [Fact]
+        public static void IntTest()
+        {
+            var list = new List<int>() { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
+            int[] sortedList = { 0, 2, 3, 4, 8, 9, 12, 15, 16, 23, 25, 34, 42, 46, 55 };
+            List<int> result = list.MergeSort();
+            Assert.True(result.SequenceEqual(sortedList));
+        }
+        [Fact]
+        public static void StringTest()
+        {
+            var list = new List<char>() { 'c', 'b', 'a', 'd' };
+            char[] sortedList = { 'a', 'b', 'c', 'd' };
+            List<char> result = list.MergeSort();
+            Assert.True(result.SequenceEqual(sortedList));
+        }
+        [Fact]
+        public static void TwoElementTest()
+        {
+            var list = new List<int>() { 1, 0 };
+            int[] sortedList = { 0, 1 };
+            List<int> result = list.MergeSort();
+            Assert.True(result.SequenceEqual(sortedList));
+        }
+        [Fact]
+        public static void OneElementTest()
+        {
+            var list = new List<int>() { 23 };
+            int[] sortedList = { 23 };
+            List<int> result = list.MergeSort();
+            Assert.True(result.SequenceEqual(sortedList));
+        }
+    }
+}
---
+++ Algorithms/Sorters/MergeSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorters
+{
+    //  stable
+    //  time complexity worst: O(n log(n)) = T(n)=2T(n/2)+n by Master Theorem
+    //  space complexity: O(n) = O(log(n)) + O(n) 合併的額外空間+call stack
+    public static class MergeSorter
+    {
+        public static List<T> MergeSort<T>(this List<T> collection, IComparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            return InternalMergeSort(collection, comparer);
+        }
+        public static List<T> InternalMergeSort<T>(List<T> collection,IComparer<T> comparer)
+        {
+            int mid = collection.Count / 2;
+            if (collection.Count < 2)
+            {
+                return collection;
+            }
+            var leftCollection = collection.GetRange(0, mid);
+            var rightCollection = collection.GetRange(mid, collection.Count - mid);
+            leftCollection = InternalMergeSort(leftCollection, comparer);
+            rightCollection = InternalMergeSort(rightCollection, comparer);
+            return Merge(leftCollection, rightCollection,comparer);
+        }
+        public static List<T> Merge<T>(List<T> left, List<T> right, IComparer<T> comparer)
+        {
+            List<T> result = new List<T>();
+            int leftIndex = 0;
+            int rightIndex = 0;
+
+            while(leftIndex < left.Count && rightIndex < right.Count)
+            {
+                if (comparer.Compare(left[leftIndex], right[rightIndex]) <= 0)
+                {
+                    result.Add(left[leftIndex++]);
+                }
+                else
+                {
+                    result.Add(right[rightIndex++]);
+                }
+            }
+
+            while (leftIndex != left.Count)
+            {
+                result.Add(left[leftIndex++]);
+            }
+
+            while (rightIndex != right.Count)
+            {
+                result.Add(right[rightIndex++]);
+            }
+
+            return result;
+        }
+    }
+
+}
